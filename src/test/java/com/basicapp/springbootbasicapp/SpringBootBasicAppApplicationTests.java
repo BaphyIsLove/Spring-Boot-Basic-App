746b0af12d74568ec91a9e3b4fe80367500e377c
baphy <baphyislove@gmail.com>
2023-07-14 03:28:43 -0600

    create project

---
+++ src/test/java/com/basicapp/springbootbasicapp/SpringBootBasicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basicapp.springbootbasicapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBasicAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
